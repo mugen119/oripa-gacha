127dbc93355c0a784c930a0cbb0da6c7f39b12f8
mugen119 <160013939+mugen119@users.noreply.github.com>
2025-07-18 16:42:42 +0900

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  base: '/oripa-gacha/',  // GitHubリポジトリ名に置き換える
   plugins: [react()],
 });
