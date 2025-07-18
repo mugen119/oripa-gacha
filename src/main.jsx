86b0e44aa99981a7ad78b6475c7a8087021c8f0a
mugen119 <160013939+mugen119@users.noreply.github.com>
2025-07-18 12:27:36 +0900

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+import VvvSlotMachine from "./App";
+import "./index.css"; // 必要なスタイル（Tailwind使うなら）
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <VvvSlotMachine />
   </React.StrictMode>
 );
